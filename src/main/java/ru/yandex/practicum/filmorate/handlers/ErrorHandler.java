b966e55373f381aef07196a20b79289bc391be01
Nikita <kim.nikituschcka@yandex.ru>
2022-08-19 21:51:18 +0300

    add-reviews

--- src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
@@ -3,7 +3,9 @@ package ru.yandex.practicum.filmorate.handlers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.controllers.*;
 import ru.yandex.practicum.filmorate.exceptions.DaoException;
@@ -17,7 +19,8 @@ import java.util.Map;
         UserController.class,
         GenreController.class,
         MpaController.class,
-        DirectorController.class
+        DirectorController.class,
+        ReviewController.class
 })
 public class ErrorHandler {
 
